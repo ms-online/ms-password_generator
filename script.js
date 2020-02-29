0b78a24c4c74925ac7e8bb05cdcf22e9cd1a191a
ms-online <27732357@qq.com>
2020-02-29 19:57:06 +0800

    lesson-1

---
+++ script.js
@@ -0,0 +1,98 @@
+// 第三步 获取DOM节点
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+// 第二步 创建随机函数对象
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+// 第六步 复制到剪切板事件监听
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("密码已复制到剪切板");
+});
+// 第四步 生成密码事件监听
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  resultEl.innerHTML = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// 第五步 设置generatePassword函数
+function generatePassword(lower, upper, number, symbol, length) {
+  // 1.初始化密码
+  let generatedPassword = "";
+  // 2.过滤出没有选中的密码类型
+  const typesCount = lower + upper + number + symbol;
+  const typeArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    item => Object.values(item)[0]
+  );
+  // console.log(typeArr);
+  if (typesCount === 0) {
+    return "";
+  }
+  // 3.通过循环获得每个密码并返回给存储密码的变量
+  for (let i = 0; i < length; i += typesCount) {
+    typeArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  // console.log(generatedPassword);
+  // 4.将处理后的随机密码结果进行保存再返回这个值
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+}
+
+// 第一步 设置随机生成函数
+
+// 随机小写
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// 随机大写
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// 随机数字
+function getRandomNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// 随机符号
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+// console.log(getRandomSymbol());
